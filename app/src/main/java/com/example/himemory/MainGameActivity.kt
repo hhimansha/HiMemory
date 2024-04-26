21f1650d548a264259426a63df4910b7e62890d4
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-04-26 20:17:22 +0530

    memory board adapter class and the main game views

---
+++ app/src/main/java/com/example/himemory/MainGameActivity.kt
@@ -0,0 +1,37 @@
+package com.example.himemory
+
+import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainGameActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard : RecyclerView
+    private lateinit var tvNumMoves : TextView
+    private lateinit var tvNumPairs : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_game)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
+    }
+}
---
+++ app/src/main/java/com/example/himemory/MemoryBoardAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.himemory
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import android.content.Context
+import android.view.View
+
+class MemoryBoardAdapter(private val context: Context,  private val numPieces: Int) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
